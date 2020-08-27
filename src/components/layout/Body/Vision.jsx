7d1a6fc1d4e2a63e0d23c609e5d849658348d274
heretodesign <senwelo.works@gmail.com>
2020-08-27 20:12:18 +0800

    mobile optimazation our Vision component

--- src/components/layout/Body/Vision.jsx
+++ src/components/layout/Body/Vision.jsx
@@ -1,73 +1,67 @@
 import React from 'react'
-import { Link } from "react-router-dom"
-// import { Button, Columns} from "react-bulma-components/full"
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 import imgOffice from '../../../assets/emily-rusch-6dCicDMhMjk-unsplash.png'
+import styled from 'styled-components'
+
+const VisionParagraph = styled.p`
+  font-size: 1.2rem;
+  margin-top: 10px;
+  color: #fff;
+  font-family: 'Open Sans', sans-serif;
+  @media (max-width: 768px) { 
+    text-align: center;
+    color: #fff;
+    font-family: 'Open Sans', sans-serif;
+	}
+`
+const VisionTitle = styled.p`
+  font-size: 2.6rem;
+  color: #fff;
+  font-family: 'Open Sans', sans-serif;
+
+  @media (max-width: 768px) { 
+    color: #fff;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 2rem;
+    text-align: center;
+	}
+`
+const VisionGet = styled.p`
+  font-size: 2.6rem;
+  margin-top: 0px;
+  color: #011240;
+  font-family: 'Open Sans', sans-serif;
+`
 
 const Vision = () => {
   return (
     <>
-     <section className="section is-paddingless-horizontal" id="aboutPage">
+      <section className="section is-paddingless-horizontal" id="aboutPage">
         <div className="container grid is-large" id="contAboutPage">
-            <div className="firstsection">
-                <div className="content">
-                    {/* <p className="subtitle is-6 has-text-left has-text-grey has-text-weight-semibold is-uppercase">Your sgdfgsdf logistics solution</p> */}
-                </div>
+            <div className="firstssection">
                 <div className="content">
                   <div class="columns">
                     <div class="column is-half">
                       <img className="content" id="imgCover" src={imgOffice} alt="office" />
                     </div>
                     <div class="column">
-                      <div className="content" id="contentSide">
-                        <h3 className="subtitle is-6 has-text-left is-uppercase" id="headTitle">Why we exist</h3>
-                        <p className="subtitle has-text-left is-2" style={colorStyle}>Our Vision & Mandate</p>
-
-                        <p className="has-text-left" style={paragraphStyle}>
+                      <div className="content">
+                        <VisionGet className="subtitle is-6 has-text-left is-uppercase" id="headTitle">Why we exist</VisionGet>
+                        <VisionTitle className="subtitle has-text-left is-2">Our Vision & Mandate</VisionTitle>
+                        <VisionParagraph className="has-text-left">
                             Our endevour is to establish and entrench Seth Energy Resources as a leading force in the "mid"
-                             and "down-stream" sector of the Oil and Gas industry. <br /><br />Our success has been based on uncompromising honesty, integrity, reliability, flexibility
-                             , customer orientation, quality, value and timing and we will continue to base our business on these principals going forward. 
-                        </p>
+                            and "down-stream" sector of the Oil and Gas industry. <br /><br />Our success has been based on uncompromising honesty, integrity, reliability, flexibility
+                            , customer orientation, quality, value and timing and we will continue to base our business on these principals going forward. 
+                        </VisionParagraph>
                       </div>
                     </div>
                   </div>
                 </div>
-             </div>
+              </div>
         </div>
-     </section>
+      </section>
     </>
   );
-}
-
-const headerStyle = {
-  fontSize: '2.6rem',
-  marginTop: '80px',
-  color: '#011240'
-}
-const paragraphStyle = {
-  fontSize: '1.2rem',
-  marginBottom: '0px',
-  color: '#fff'
-}
-const colorStyle = {
-  color: '#fff'
-}
-
-const parentDiv = {
-  marginTop: '80px',
-  marginLeft: '70px',
-  marginRight: '50px'
-}
-
-const founderStyle = {
-  padding: '0px 0px',
-  background: '#001240',
-  // background: '#F6F9FF',
-  color: '#011240'
-}
-
-const innerStyle = {
-  height: '500px'
 }
 
 export default Vision;
